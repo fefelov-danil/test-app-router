f7fa3b64c284ef425f08e4a5a0be56e46f0699a2
Daniil Fefelov <fefelov.danil@gmail.com>
2023-12-01 22:08:29 +0400

    Rick and Morty

---
+++ src/components/info.tsx
@@ -0,0 +1,18 @@
+import {ICharacter} from "@/api";
+import {Character} from "@/components/character";
+
+interface Props {
+  characters: ICharacter
+}
+
+export const Info = ({characters}: Props) => {
+
+  return (
+    <>
+      <h2 className="text-center my-5">Количество пресонажей: {characters.info.count}</h2>
+      <div className="grid grid-cols-3 gap-4 max-w-[1000px] mx-auto">
+        {characters.results.map((character, i) => <Character key={i} character={character} />)}
+      </div>
+    </>
+  )
+}
